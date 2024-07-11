797af18123714945e3df5a5c414d1960fe8c8f2c
Predrag Đokić <predragdj@terieng.co.rs>
2024-07-11 11:53:51 +0200

    Set "Spring Cloud Config Client".

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@
 # Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 # Click nbfs://nbhost/SystemFileSystem/Templates/Other/Dockerfile to edit this template
 
-FROM openjdk:24-jdk-oraclelinux8
+FROM alpine/java:21.0.3-jre
+RUN apk update && apk add --no-cache curl
 RUN mkdir app
 ADD target/service-a-0.0.1-SNAPSHOT.jar app/app.jar
 EXPOSE 8080
--- src/main/java/rs/ac/bg/fon/mas/service_a/ServiceAApplication.java
+++ src/main/java/rs/ac/bg/fon/mas/service_a/ServiceAApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FonServiceAApplication {
+public class ServiceAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FonServiceAApplication.class, args);
+		SpringApplication.run(ServiceAApplication.class, args);
 	}
 
 }
